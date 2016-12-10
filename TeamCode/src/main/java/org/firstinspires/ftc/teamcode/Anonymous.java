cf6b6ec48a64daef48e6bc8a9066b5040466e462
7090 <teamfar7090@gmail.com>
2016-12-10 13:59:05 -0500

    Testing 123 12/10/2016 1:58

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anonymous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anonymous.java
@@ -1,41 +1,99 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
+/**\
  * Created by Team FAR on 11/12/16
  */
-
-public class Anonymous extends LinearOpMode{
+@Autonomous(name = "Anonymous")
+public class Anonymous extends LinearOpMode {
 
     DcMotor leftDrive;
     DcMotor rightDrive;
-    ColorSensor sensor_color;
+    DcMotor loader2;
+    DcMotor loader;
+    DcMotor shooter;
 
-    public void runOpMode (){
+    public void runOpMode() throws InterruptedException {
         leftDrive = hardwareMap.dcMotor.get("leftDrive");
         rightDrive = hardwareMap.dcMotor.get("rightDrive");
-        sensor_color = hardwareMap.colorSensor.get("sensor_color");
-        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-
-
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        loader = hardwareMap.dcMotor.get("loader");
+        loader2 = hardwareMap.dcMotor.get("loader2");
+        shooter = hardwareMap.dcMotor.get("shooter");
 
         waitForStart();
 
-        rightDrive.setPower(.5);
-        leftDrive.setPower(.5);
+        rightDrive.setPower(-1);
+        leftDrive.setPower(-1);
 
-        if (sensor_color.red()){
-            rightDrive.setPower(1);
-            leftDrive.setPower(1);
+        ElapsedTime eTime = new ElapsedTime();
+
+        eTime.reset();
+
+        while (eTime.time() < .5) {
         }
 
-        else {
-            rightDrive.setPower(.5);
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+
+        shooter.setPower(1);
+        try {
+            Thread.sleep(1500);
         }
+        catch ( Error e)
+        {
+
+        }
+
+
+        loader2.setPower(1);
+        try {
+            Thread.sleep(2000);
+        }
+        catch (Error e) {
+
+        }
+
+        shooter.setPower(0.0);
+        loader2.setPower(0.0);
+
+        rightDrive.setPower(-1);
+        leftDrive.setPower(-1);
+
+
+        eTime.reset();
+
+        while (eTime.time() < 2.5) {
+        }
+
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
     }
 }
+    /*{
+        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        shooter.setPower(1);
+        loader2.setPower(1);
+        }
+
+        {
+        eTime.reset();
+
+        while(eTime.time() < 2.5)
+
+        shooter.setPower(0);
+        loader2.setPower(0);
+
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+        }
+
+    }
+}*/
