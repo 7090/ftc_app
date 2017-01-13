ebd847d91fb1fe0a26e148791aed67d805d85702
7090 <teamfar7090@gmail.com>
2017-01-13 17:49:42 -0500

    1/13/17
    Comments Made
    
    ~veronica

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Psynonemous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Psynonemous.java
@@ -30,29 +30,27 @@ public class Psynonemous extends LinearOpMode {
         loader2.setDirection(DcMotorSimple.Direction.REVERSE);
         shooter.setDirection(DcMotorSimple.Direction.REVERSE);
 
+       /* This is where i call all of my methods, meaning that this is the area where i tell the
+       robot specifically what i want it to do */
+
         waitForStart();
 
         int i;
 
-        for (i = 0; i < 10; i++) {
+        for (i = 0; i < 16; i++) {     // This one calls the DriveForward command, making it drive
+            DriveForward();            // forward for 16 iterations, meaning it will go through
+        }                              // that specific method 16 times.
+
+        for (i = 0; i < 1; i++) {       // this calls the method ActivateRight, by doing this it
+            ActivateRight();            // turns the robot to the right.
+        }
+
+       PewPewShooter();                 // This calls the method PewPewShooter, which activates the
+                                        // shooting mechanisms, launching the particles.
+
+        for (i = 0; i < 16; i++) {      // this drives the robot forward, for the remaining time.
             DriveForward();
         }
-
-        for (i = 0; i < 10; i++) {
-            ActivateLeft();
-        }
-
-        for (i = 0; i < 10; i++) {
-            DriveBack();
-        }
-
-        for (i = 0; i < 10; i++) {
-            ActivateRight();
-        }
-
-        PewPewShooter();
-
-
 
     }
 
@@ -66,23 +64,39 @@ public class Psynonemous extends LinearOpMode {
 
     }
 
-    public void PewPewShooter (){
+    /**
+     * Method Name: PewPewShooter
+     * Purpose: Launching Particles
+     * Date Modified: 1/13/17
+     */
+    public void PewPewShooter (){   // this creates a public method.
 
-        shooter.setPower(1);
-        try {
-            Thread.sleep(1500);
+        shooter.setPower(1);        // this sets the shooter power to 1 (full power)
+
+        try {   // Try Catch is used because if it does not sleep, it should continue to the next
+                // step
+
+            Thread.sleep(1500);    // this makes it stop for 1.5 seconds, before continuing.
+
+
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            e.printStackTrace();        // What this does is print what causes the error.
+
 
         }
-        loader2.setPower(1);
+        loader2.setPower(1);       // this sets the loader power to 1 (full power)
 
-        try {
-            Thread.sleep(2000);
+        try {       // Try Catch is used because if it does not sleep, it should continue to the next
+            // step
+
+
+            Thread.sleep(2500);   // this makes it stop for 1.5 seconds, before continuing.
+
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            e.printStackTrace();        // What this does is print what causes the error.
         }
-        shooter.setPower(0.0);
+
+        shooter.setPower(0.0);      // this stops the motors.
         loader2.setPower(0.0);
     }
 
@@ -103,67 +117,102 @@ public class Psynonemous extends LinearOpMode {
 
     }
 
-    public void DriveForward (){
 
-        rightDrive.setPower(1);
+    /**
+     * Method Name: DriveForward
+     * Purpose: drive forward
+     * Date Modified: 1/13/17
+     */
+    public void DriveForward (){    // This creates a Public Method
+
+        rightDrive.setPower(1);     // this sets both drive motors to full power.
         leftDrive.setPower(1);
 
-        ElapsedTime eTime = new ElapsedTime();
+        ElapsedTime eTime = new ElapsedTime();      // this is a time parameter, telling it to drive
+                                                    // for .1 seconds,
 
         eTime.reset();
 
         while (eTime.time() < .1) {
         }
 
-        leftDrive.setPower(0.0);
+        leftDrive.setPower(0.0);            // This stops the driving motors.
         rightDrive.setPower(0.0);
 
     }
 
-    public void DriveBack (){
 
-        rightDrive.setPower(-1);
+    /**
+     * Method Name: DriveBack
+     * Purpose: Driving Backwards
+     * Date Modified: 1/13/17
+     */
+    public void DriveBack (){       // This Creates a public method
+
+        rightDrive.setPower(-1);    // This sets the Drive motors, to move in reverse.
         leftDrive.setPower(-1);
 
-        ElapsedTime eTime = new ElapsedTime();
+        ElapsedTime eTime = new ElapsedTime();      // this is a time parameter, telling it to drive
+                                                    // for .1 seconds,
 
         eTime.reset();
 
         while (eTime.time() < .1) {
         }
 
-        leftDrive.setPower(0.0);
+        leftDrive.setPower(0.0);        // This stops the driving motors.
         rightDrive.setPower(0.0);
     }
 
+
+
+
+
+
+    /**
+     * Method Name: ActivateLeft
+     * Purpose: Turning Left
+     * Date Modified: 1/13/17
+     */
     public void ActivateLeft (){
 
-        leftDrive.setPower(1);
-        rightDrive.setPower(0);
+        leftDrive.setPower(1);      //This sets one motor to turn forward and the other to turn
+        rightDrive.setPower(-1);   // backwards, making a 0 point turn
 
-        ElapsedTime eTime = new ElapsedTime();
+
+        ElapsedTime eTime = new ElapsedTime();      // this is a time parameter, telling it to drive
+                                                    // for .1 seconds,
 
         eTime.reset();
 
         while (eTime.time() < .1) {
         }
 
-        leftDrive.setPower(0);
+        leftDrive.setPower(0);      // This stops drive motors
+        rightDrive.setPower(0);
     }
 
+
+    /**
+     * Method Name: ActivateRight
+     * Purpose: Turning Right
+     * Date Modified: 1/13/17
+     */
     public void ActivateRight (){
 
-        leftDrive.setPower(0);
-        rightDrive.setPower(1);
+        leftDrive.setPower(-1);         //This sets one motor to turn forward and the other to turn
+        rightDrive.setPower(1);         // backwards, making a 0 point turn
 
-        ElapsedTime eTime = new ElapsedTime();
+        ElapsedTime eTime = new ElapsedTime();       // this is a time parameter, telling it to drive
+        // for .1 seconds,
 
         eTime.reset();
 
         while (eTime.time() < .1) {
         }
 
-        leftDrive.setPower(0);
+        leftDrive.setPower(0);      // This Stops the Drive motors
+        rightDrive.setPower(0);
     }
 
 
